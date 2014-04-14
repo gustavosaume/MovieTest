4eaba9efb7fe754d96de054331f6d17cd512ebce
Gustavo Saume <gustavosaume@gmail.com>
2014-04-14 11:24:51 -0400

    Adds user interactions

--- js/main.js
+++ js/main.js
@@ -1,1 +1,19 @@
+$(document).ready(function() {
+  $(".movies > .movie").click(function() {
+    $(".movie").removeClass("selected");
+    $(this).addClass("selected");
+  });
+});
 
+// (function(){
+//   var current = null;
+//   function select() {
+
+//   }
+//   function change() {
+
+//   }
+//   function verify() {
+
+//   }
+// })();
